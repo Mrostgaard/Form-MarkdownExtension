88f64141a4a5e98687269dcbb6493965ea0c5f0c
Mark Mortensen <mrostgaard@gmail.com>
2020-08-04 19:45:18 +0200

    initial commit!

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+setup(
+    name='formextension',
+    version='1.0',
+    py_modules=['formextension'],
+    install_requires = ['markdown>=3.0'],
+    author_email='mrostgaard@protonmail.com',
+    url='https://blog.rostgaard.it',
+    author = 'Mark Rostgaard',
+)
