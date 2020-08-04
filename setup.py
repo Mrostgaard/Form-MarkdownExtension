15e16cb720398b3947764c116058a9ee81d59781
Mark Mortensen <mrostgaard@gmail.com>
2020-08-04 20:27:53 +0200

    added license

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 setup(
     name='formextension',
     version='1.0',
+    author = 'Mark Rostgaard',
     py_modules=['formextension'],
     install_requires = ['markdown>=3.0'],
     author_email='mrostgaard@protonmail.com',
-    url='https://blog.rostgaard.it',
-    author = 'Mark Rostgaard',
+    url='https://github.com/Mrostgaard/Form-MarkdownExtension',
 )
